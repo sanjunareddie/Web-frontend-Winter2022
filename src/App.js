020654994dd23fe1fcbc2b7e791e9c8d0b391d91
Rishabh Domadiya <rishabh.canada5@gmail.com>
2022-03-29 14:27:35 -0300

    Manage Houses/Rentals code created

--- src/App.js
+++ src/App.js
@@ -6,21 +6,34 @@ import NavBarBeforeLogin from "./components/NavBarBeforeLogin";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 
+import HousesList from "./components/HousesList";
+import AddHouse from "./components/AddHouse";
+import HouseDetail from "./components/HouseDetail";
+import HouseEdit from "./components/HouseEdit";
+import DeleteHouse from "./components/DeleteHouse";
+
 function App() {
   return (
     <>
       <NavBarBeforeLogin />
 
       <div className="App">
-        <div className="outer">
-          <div className="inner">
+        {/* <div className="outer"> */}
+          {/* <div className="inner"> */}
             <Routes>
               <Route exact path="/" element={<SignIn />} />
               <Route path="/sign-in" element={<SignIn />} />
               <Route path="/sign-up" element={<SignUp />} />
+
+              {/* Houses */}
+              <Route path="/houses" element={<HousesList />}></Route>
+              <Route path="/add-house" element={<AddHouse />}></Route>
+              <Route path="/house/:id" element={<HouseDetail />}></Route>
+              <Route exact path="/house/:id/edit" element={<HouseEdit />}></Route>
+              <Route exact path="/house/:id/delete" element={<DeleteHouse />}></Route>
             </Routes>
-          </div>
-        </div>
+          {/* </div> */}
+        {/* </div> */}
       </div>
     </>
   );
