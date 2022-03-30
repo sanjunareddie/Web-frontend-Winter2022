a9fca3c0f74f762da855ee5bcdf4736bc6cbe218
DHRUV OZA <dh223255@dal.ca>
2022-03-29 22:25:15 -0300

    Final Bug Fixed

--- src/components/NavBarAfterLogin/index.js
+++ src/components/NavBarAfterLogin/index.js
@@ -36,12 +36,6 @@ function NavBarAfterLogin() {
             </Nav.Item>
 
             <Nav.Item className="p-2">
-              <NavLink to="/RentalForm" className="nav-text-color">
-                Rental Form
-              </NavLink>
-            </Nav.Item>
-
-            <Nav.Item className="p-2">
               <NavLink to="/UpdateApplicationStatus" className="nav-text-color">
                 Update Application Status
               </NavLink>
